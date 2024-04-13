216c09422969731247bc5313cb5ee0552073b21d
Nimoismail <nimoismail1655@gmail.com>
2024-04-13 13:30:32 +0300

    added flash message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,session
+from flask import Flask,render_template,request,redirect,url_for,session,flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def home():
     return render_template('index.html')
 
 @app.route('/login', methods=["POST","GET"])
-def index():
+def login():
     if request.method == 'POST':
         session.permanent = True
         user = request.form['nm']
@@ -32,8 +32,9 @@ def user(usr):
 @app.route('/logout')
 def logout():
     session.pop('user', None)
-    return render_template('login')
+    flash("You have been loggedout!", "info")
+    return redirect(url_for('login'))
 
 
 if __name__== '__main__':
- app.run(debug=True)
+ app.run(debug=True, port=9000)
